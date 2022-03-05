7c87551eb9cc3a0675fec9ee0d917397fef7a182
sowinskipatryk <91700001+sowinskipatryk@users.noreply.github.com>
2022-03-06 00:56:54 +0100

    display delay for convenience

--- battleships_oop.py
+++ battleships_oop.py
@@ -7,6 +7,8 @@ TARGET_MISSED = ' . '
 TARGET_HIDDEN = ' | '
 TARGET_REVEALED = ' O '
 
+SHIPS = [6, 4, 4, 3, 3, 2, 2]
+
 
 class Battlefield:
 
@@ -59,25 +61,36 @@ class Battlefield:
             return True
 
     def generate_battlefield(self):
-        ships = [6, 4, 4, 3, 3, 2, 2]
+        opps_ships = SHIPS[:]
 
-        while ships:
+        while opps_ships:
             direction = random.choice(['H', 'V'])
             row, col = [random.randint(0, 9) for _ in range(2)]
-            if not self.is_placement_correct(row, col, ships[0], direction):
+            if not self.is_placement_correct(row, col, opps_ships[0], direction):
                 continue
             else:
-                self.place_ship(row, col, ships.pop(0), direction)
+                self.place_ship(row, col, opps_ships.pop(0), direction)
 
+    def all_ships_destroyed(self):
+        flatten_battlefield = [x for y in self.battlefield for x in y]
+        if not any(x == TARGET_REVEALED for x in flatten_battlefield):
+            return True
 
-def is_good_move(row, col, obj):
-    if row not in range(10) or col not in range(10):
-        print("This is not a valid move.")
-        print("You have already shot at this field or it is not on the battlefield.")
-        return False
-    if obj.battlefield[row][col] == TARGET_HIT or obj.battlefield[row][col] == TARGET_MISSED:
-        return False
-    return True
+    def hit(self, row, col):
+        if self.battlefield[row][col] == TARGET_REVEALED:
+            self.battlefield[row][col] = TARGET_HIT
+            return True
+        else:
+            self.battlefield[row][col] = TARGET_MISSED
+
+    def is_correct_move(self, row, col):
+        if row not in range(10) or col not in range(10):
+            print("This is not a valid move.")
+            print("You have already shot at this field or it is not on the battlefield.")
+            return False
+        if self.battlefield[row][col] == TARGET_HIT or self.battlefield[row][col] == TARGET_MISSED:
+            return False
+        return True
 
 
 def convert_field_to_num(field):
@@ -86,21 +99,7 @@ def convert_field_to_num(field):
     return letter_id, number
 
 
-def hit(obj, row, col):
-    if obj.battlefield[row][col] == TARGET_REVEALED:
-        obj.battlefield[row][col] = TARGET_HIT
-        return True
-    else:
-        obj.battlefield[row][col] = TARGET_MISSED
-
-
-def all_ships_destroyed(obj):
-    flatten_battlefield = [x for y in obj.battlefield for x in y]
-    if not any(x == TARGET_REVEALED for x in flatten_battlefield):
-        return True
-
-
-ships = [6, 4, 4, 3, 3, 2, 2]
+players_ships = SHIPS[:]
 
 
 def main():
@@ -110,59 +109,69 @@ def main():
     opp = Battlefield()
     moves = Battlefield()
     game_finished = False
-    while ships:
+    while players_ships:
         player.show_battlefield()
-        first_field = input(f'Enter the first field of your ship (size {ships[0]}): ')
-        direction = input('\nEnter the direction of your ship - (H)orizontal / (V)ertical: ')
+        first_field = input(f'Enter the first field of your ship (size {players_ships[0]}): ')
+        direction = input('\nEnter the direction of your ship - (H)orizontal / (V)ertical: \n')
         row, col = first_field[0].upper(), int(first_field[1:]) - 1
         row = [x for x in string.ascii_uppercase[:10]].index(row)
-        if not player.is_placement_correct(row, col, ships[0], direction):
+        if not player.is_placement_correct(row, col, players_ships[0], direction):
             print('\nThe ships cannot overlap and must fit on the battlefield.')
             print('Choose another place for your ship.\n')
             continue
         else:
-            player.place_ship(row, col, ships.pop(0), direction)
+            player.place_ship(row, col, players_ships.pop(0), direction)
+    time.sleep(1.5)
     print('Your final ship placement:\n')
+    time.sleep(1.5)
     player.show_battlefield()
     opp.generate_battlefield()
     while not game_finished:
         player_turn = True
         while player_turn:
-            print('Moves battlefield:')
+            time.sleep(1)
+            print('Your turn: \n')
+            time.sleep(0.5)
             moves.show_battlefield()
             row, col = convert_field_to_num(input('Enter the field to shoot at: '))
-            if not is_good_move(row, col, moves):
+            if not moves.is_correct_move(row, col):
                 continue
-            if hit(opp, row, col):
+            if opp.hit(row, col):
                 moves.battlefield[row][col] = TARGET_HIT
-                print('Great! It\'s a hit!')
-                time.sleep(2)
-                if all_ships_destroyed(opp):
+                print('\nGreat! It\'s a hit!\n')
+                time.sleep(1.5)
+                if opp.all_ships_destroyed():
                     print('Player wins!')
                     player_turn = False
                     opp_turn = False
                     game_finished = True
             else:
                 moves.battlefield[row][col] = TARGET_MISSED
-                print('You missed this time.')
+                time.sleep(0.5)
+                print('\nYou missed this time.\n')
                 player_turn = False
                 opp_turn = True
         while opp_turn:
+            time.sleep(1.5)
+            print('Opponent\'s turn.\n')
             row, col = [random.randint(0, 9) for _ in range(2)]
-            if not is_good_move(row, col, player):
+            if not player.is_correct_move(row, col):
                 continue
-            if hit(player, row, col):
+            if player.hit(row, col):
+                time.sleep(1.5)
                 print('Opponent has hit your ship!')
-                time.sleep(2)
-                if all_ships_destroyed(player):
+                time.sleep(1.5)
+                if player.all_ships_destroyed():
                     print('Opponent wins!')
                     opp_turn = False
                     game_finished = True
             else:
-                print('Opponent missed.')
+                time.sleep(1.5)
+                print('Opponent missed.\n')
                 opp_turn = False
+            time.sleep(0.5)
             player.show_battlefield()
-            time.sleep(2)
+            time.sleep(0.5)
 
 
 if __name__ == '__main__':
